66de6e2e65649649d41c54b6d3156ca40970a42f
Bradkibs <Bradleykibs@gmail.com>
2023-08-01 16:54:08 +0300

    basic flask setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,9 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def output():
+def main():
+    """entry point"""
     return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run()
